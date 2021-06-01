52df62c7940e3b4b5bf26f0d67765d20ff945311
serginaAleksandra <sergina.al.al@gmail.com>
2021-06-01 13:58:02 +0700

    first commit

---
+++ lab15/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lavender;
+            this.label1.Location = new System.Drawing.Point(899, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label2.Location = new System.Drawing.Point(413, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 23);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Lucida Fax", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.button1.Location = new System.Drawing.Point(396, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 73);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find out the weather";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lavender;
+            this.label3.Location = new System.Drawing.Point(855, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sun";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Image = global::lab15.Properties.Resources.cloud;
+            this.pictureBox1.Location = new System.Drawing.Point(284, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(424, 273);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(983, 535);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lab15/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DateTime date1 = new DateTime(0, 0);
+        DateTime date2 = new DateTime(1, 1, 1);
+        int time = 0, knull = 0;
+        double Tao = 0;
+        double[,] Q = new double[3, 2] { { 0.75, 0.25 }, { 0.5, 0.5 }, { 0.2, 0.8 } };
+        double[] Statistika = new double[3];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            date2 = date2.AddHours(3);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            date1 = date1.AddHours(1);
+            label1.Text = date1.ToString("dd.HH");
+
+            Random a = new Random();
+            double r = a.NextDouble();
+
+
+            if (date1.ToString("HH") == date2.ToString("HH"))
+            {
+                if (knull == 0)
+                {
+                    label2.Text = "It's sunny now";
+                    if (r - Q[0, 0] <= 0) knull = 1;
+                    else knull = 2;
+                    Tao = Math.Log(r) / (-0.4);
+                    time += Convert.ToInt32(Tao);
+                    Statistika[knull]++;
+                    date2 = date1.AddHours(Tao);
+                    pictureBox1.Image = lab15.Properties.Resources.sun;
+                }
+                else
+                if (knull == 1)
+                {
+                    label2.Text = "It's raining now";
+                    if (r - Q[1, 0] <= 0) knull = 0;
+                    else knull = 2;
+                    Tao = Math.Log(r) / (-0.8);
+                    time += Convert.ToInt32(Tao);
+                    Statistika[knull]++;
+                    date2 = date1.AddHours(Tao);
+                    pictureBox1.Image = lab15.Properties.Resources.rain;
+                }
+                else
+                if (knull == 2)
+                {
+                    label2.Text = "It's cloudy now";
+                    if (r - Q[2, 0] <= 0) knull = 0;
+                    else knull = 1;
+                    Tao = Math.Log(r) / -0.5;
+                    time += Convert.ToInt32(Tao);
+                    Statistika[knull]++;
+                    date2 = date1.AddHours(Tao);
+                    pictureBox1.Image = lab15.Properties.Resources.cloud;
+                }
+            }
+            
+            if ((date1.ToString("HH") == "00") & (label3.Text == "Sun")) label3.Text = "Mon";
+            else
+                if ((date1.ToString("HH") == "00") & (label3.Text == "Mon")) label3.Text = "Tue";
+            else
+                if ((date1.ToString("HH") == "00") & (label3.Text == "Tue")) label3.Text = "Wed";
+            else
+                if ((date1.ToString("HH") == "00") & (label3.Text == "Wed")) label3.Text = "Thu";
+            else
+                if ((date1.ToString("HH") == "00") & (label3.Text == "Thu")) label3.Text = "Fri";
+            else
+                if ((date1.ToString("HH") == "00") & (label3.Text == "Fri")) label3.Text = "Sat";
+            else
+                if ((date1.ToString("HH") == "00") & (label3.Text == "Sat")) label3.Text = "Sun";
+        }
+    }
+}
